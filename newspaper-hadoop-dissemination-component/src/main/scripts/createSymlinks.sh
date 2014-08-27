78db29ff6f7bdaf7027cbbbe3c5958d1102e016e
Colin Rosenthal <csr@statsbiblioteket.dk>
2014-08-27 09:55:22 +0200

    Command line tool for creating symlinks ready for review.

---
+++ newspaper-hadoop-dissemination-component/src/main/scripts/createSymlinks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+print_usage()
+{
+    echo "Usage: $(basename $0) roundtripid"
+    echo
+    echo "For example $(basename $0) B400026952016-RT1"
+}
+
+if [ $# -ne 1 ]; then
+  print_usage
+  exit 1
+fi
+
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+
+java -classpath "$SCRIPT_DIR/../conf:$SCRIPT_DIR/../lib/*" \
+ dk.statsbiblioteket.medieplatform.hadoop.SymlinkCreatorApplication $1 -c $SCRIPT_DIR/../conf/config.properties
