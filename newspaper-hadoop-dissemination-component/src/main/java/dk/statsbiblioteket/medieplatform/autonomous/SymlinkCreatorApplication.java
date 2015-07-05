ce9bf7cf0285eb66b76f4ac6495af0ee731ae95a
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-07-05 10:24:09 +0200

    Use newest framework and newest sbutil

--- newspaper-hadoop-dissemination-component/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/SymlinkCreatorApplication.java
+++ newspaper-hadoop-dissemination-component/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/SymlinkCreatorApplication.java
@@ -5,7 +5,7 @@ import dk.statsbiblioteket.doms.central.connectors.BackendMethodFailedException;
 import dk.statsbiblioteket.doms.central.connectors.EnhancedFedora;
 import dk.statsbiblioteket.doms.central.connectors.EnhancedFedoraImpl;
 import dk.statsbiblioteket.doms.central.connectors.fedora.pidGenerator.PIDGeneratorException;
-import dk.statsbiblioteket.doms.webservices.authentication.Credentials;
+import dk.statsbiblioteket.sbutil.webservices.authentication.Credentials;
 import dk.statsbiblioteket.medieplatform.hadoop.DisseminationJob;
 import dk.statsbiblioteket.medieplatform.hadoop.LinkUtils;
 import dk.statsbiblioteket.medieplatform.hadoop.SymlinkCreatorReducer;
